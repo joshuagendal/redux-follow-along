9da85834f17313924ae0216284a0cc0d9b751e5f
JGendal <jgendal@JGendals-MacBook-Pro.local>
2018-06-13 14:27:53 -0600

    Fetched initial data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,5 +1,5 @@
 import { getInitialData } from '../utils/api';
-import { receiverUsers } from '../actions/users';
+import { receiveUsers } from '../actions/users';
 import { receivePolls } from '../actions/polls';
 import { setAuthedUser } from '../actions/authedUser';
 
@@ -10,13 +10,11 @@ const handleInitialData = () => {
 	return (dispatch) => {
 		return getInitialData()
 			.then(({users, polls}) => {
-				dispatch(receiverUsers(users));
+				dispatch(receiveUsers(users));
 				dispatch(receivePolls(polls));
 				dispatch(setAuthedUser(AUTHED_ID));
 			})
 	}
 };
 
-module.exports = {
-	handleInitialData
-}
+export default handleInitialData;
--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,5 +1,5 @@
 const logger = (store) => (next) => (action) => {
-  console.group(actin.type);
+  console.group(action.type);
   console.log('The action: ', action);
   const returnValue = next(action);
   console.log('The new state: ', store.getState());
